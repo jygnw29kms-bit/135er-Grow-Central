f82ba429c5828505cd749e8f8bb016e367e612c1
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-08-13 13:15:42 +0200

    Register protected camera API in appliance entry point

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,6 +1,8 @@
 """Production entry point for the Raspberry Pi appliance."""
 from app.main import app
+from app.camera import router as camera_router
 from app.gui_auth import GuiAuthMiddleware, router as gui_auth_router
 
 app.add_middleware(GuiAuthMiddleware)
 app.include_router(gui_auth_router)
+app.include_router(camera_router)
